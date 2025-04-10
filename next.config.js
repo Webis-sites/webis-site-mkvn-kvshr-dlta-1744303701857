06f7004900fbf61a4bc2febaaba73eac5131aed3
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-10 20:02:46 +0300

    Update next.config.js with domain cdn.jsdelivr.net

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com', 'localhost', 'yourgymwebsite.com', 'maps.google.com'],
+    domains: ['images.unsplash.com', 'via.placeholder.com', 'localhost', 'yourgymwebsite.com', 'maps.google.com', 'cdn.jsdelivr.net'],
   },
 };
 
